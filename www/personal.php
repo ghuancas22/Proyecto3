b20a5ab4e767fe5b1320db064f4bdc30f24622c7
rmihailescu22 <114581766+rmihailescu22@users.noreply.github.com>
2024-04-30 15:36:04 +0000

    Personal Login Robert

--- www/personal.php
+++ www/personal.php
@@ -7,13 +7,21 @@ if ($mysqli->connect_error) {
   die("Error de conexión a la base de datos: " . $mysqli->connect_error);
 }
 ?>
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Iniciar sesión en Hotel Moro</title>
+</head>
 <body>
-    <h1>Personal del hotel</h1>
-    <h3>Iniciar sesión</h3>
-    <?php
-    
-    
-    ?>
+    <h1>Hotel Moro</h1>
+    <h2>Iniciar sesión</h2>
+    <h3>Personal</h3>
+    <form action="login_personal.php" method="post">
+        <label for="username">Usuario:</label>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Iniciar sesión como personal">
+    </form>
 </body>
 </html>
