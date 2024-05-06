18ee9d91a131977e22581c9161ca1b9929eef8f8
njhusain22 <115105812+njhusain22@users.noreply.github.com>
2024-05-06 17:12:20 +0000

    Naila

--- www/personal.php
+++ www/personal.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
+<<<<<<< HEAD
     <title>Iniciar sesión en El Indio</title>
     <style>
+=======
+    <title>Iniciar sesión en Hotel Moro</title>
+    <style> /* Se añade estilo CSS para aplicar al contenido de la página*/
+>>>>>>> e386e32 (Naila)
         body {
-            font-family: Arial, sans-serif;
+            font-family: Arial, sans-serif; 
             background-color: #f1f1f1;
             margin: 0;
             padding: 0;
         }
 
-        .container {
-            max-width: 600px;
-            margin: 50px auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 10px;
+        .container { /*  Se Define un contenedor */
+            max-width: 600px; /* ancho máximo de 600 píxeles*/
+            margin: 50px auto; /* centrado horizontalmente*/
+            padding: 20px; 
+            background-color: #fff;/* fondo blanco*/
+            border-radius: 10px; /*bordes redondeados*/
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
-
+        /*Se establece el color del título principal */
         h1 {
             text-align: center;
             color: #1f0469;
@@ -33,44 +38,44 @@
             margin-bottom: 10px;
             color: #1f0469;
         }
-
+        /*Se define el estilo para el botón de envío*/
         input[type="text"],
         input[type="email"],
         input[type="password"],
         input[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-sizing: border-box;
+            width: 100%; /*El ancho de los campos se ajusta al 100% del contenedor.*/
+            padding: 10px; /* Se agrega un espacio de 10 píxeles alrededor del contenido dentro de los campo*/
+            margin-bottom: 20px; /* Se agrega un margen inferior de 20 píxeles a los campos*/
+            border: 1px solid #ccc; /*Se define un borde de 1 píxel con un color alrededor de los campos */
+            border-radius: 5px; /* Se aplican bordes redondeados a los campos */
+            box-sizing: border-box; /*Se asegura que el tamaño total del campo incluya el relleno y el borde*/
         }
 
-        input[type="submit"] {
-            background-color: #1f0469;
-            color: white;
-            font-weight: bold;
-            cursor: pointer;
+        input[type="submit"] { /*Se establecen los estilos específicos para el botón de envío*/
+            background-color: #1f0469; /*Se establece color del fondo*/
+            color: white; /*texto en blanco*/
+            font-weight: bold; /*Texto en negrita*/
+            cursor: pointer; /* Se cambia el cursor a una mano cuando se pasa sobre el botón*/
         }
 
-        input[type="submit"]:hover {
-            background-color: #230770;
+        input[type="submit"]:hover { /*Se define el estilo cuando el cursor pasa sobre el botón de envío*/
+            background-color: #230770; /*Se establece el color del fondo*/
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1>El Indio</h1>
-        <h2>Iniciar sesión</h2>
-        <h3>Personal</h3>
-        <form action="login_personal.php" method="post">
-            <label for="username">Usuario:</label>
-            <input type="text" id="username" name="username">
+    <div class="container"> <!-- Se crea un contenedor con una classe "container"-->
+        <h1>El Indio</h1> <!--Se muestra un título principal “El Indio”-->
+        <h2>Iniciar sesión</h2> <!--subtitulo-->
+        <h3>Personal</h3> <!--subtitulo-->
+        <form action="login_personal.php" method="post"> <!--Se define un formulario que enviará los datos al archivo “login_personal.php” utilizando el método POST-->
+            <label for="username">Usuario:</label> <!-- Se añade una etiqueta para el campo de entrada de usuario -->
+            <input type="text" id="username" name="username"> <!--Campo de entrada de texto para el nombre de usuario.-->
             
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password">
+            <label for="password">Contraseña:</label> <!--Etiqueta para el campo de entrada de contraseña-->
+            <input type="password" id="password" name="password"> <!--Campo de entrada de contraseña-->
             
-            <input type="submit" value="Iniciar sesión como personal">
+            <input type="submit" value="Iniciar sesión como personal"> <!--Botón de envío con el texto “Iniciar sesión como personal”-->
         </form>
     </div>
 </body>
