ddf2532bef5ec64841b3dcfef4d68d336d9f3653
rmihailescu22 <114581766+rmihailescu22@users.noreply.github.com>
2024-05-06 16:04:19 +0000

    Robert

--- www/bbdd.php
+++ www/bbdd.php
@@ -1,65 +1,132 @@
-<?php 
-// Conexión a la base de datos MySQL
-$mysqli = new mysqli("mysql", "admin", "admin123", "elindio");
-
-// Verificar la conexión
-if ($mysqli->connect_error) {
-  die("Error de conexión a la base de datos: " . $mysqli->connect_error);
-}
-?>
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Hotel El Indio</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #1f0469;
+        }
+
+        .section {
+            margin-bottom: 40px;
+        }
+
+        .client, .employee {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            padding: 15px;
+            background-color: #fff;
+        }
+
+        .client div, .employee div {
+            flex: 0 0 48%;
+            margin-bottom: 10px;
+        }
+
+        .client h2, .employee h2 {
+            margin-top: 0;
+            color: #1f0469;
+        }
+
+        .client h3, .employee h3 {
+            margin-bottom: 5px;
+            color: #333;
+        }
+    </style>
+</head>
 <body>
-<h1>Clientes : </h1>
-<?php
-// Consulta para obtener los datos de la tabla 'cliente'
-    $query_cliente = "SELECT * FROM cliente";
-    $result_cliente = $mysqli->query($query_cliente);
+    <div class="container">
+        <div class="section">
+            <h1>Clientes:</h1>
+            <?php
+            // Conexión a la base de datos MySQL
+            $mysqli = new mysqli("mysql", "admin", "admin123", "elindio");
 
-    // Mostrar resultados
-    if ($result_cliente->num_rows > 0) {
-        echo '<p>Total de filas: ' . $result_cliente->num_rows . '</p>';
-        while ($row_cliente = $result_cliente->fetch_assoc()) {
-            echo '<div style="display: inline-block; margin-right: 40px;">';
-            echo 'DNI: ' . $row_cliente['DNI'] . '<br>';
-            echo 'Nombre: ' . $row_cliente['nombre'] . '<br>';
-            echo 'Email: ' . $row_cliente['email'] . '<br>';
-            echo 'Cantidad de Personas: ' . $row_cliente['cantidad_personas'] . '<br>';
-            echo 'Habitacion: ' . $row_cliente['habitacion'] . '<br>';
-            echo 'fecha de Entrada: ' . $row_cliente['fecha_entrada'] . '<br>';
-            echo 'Fecha de Salida: ' . $row_cliente['fecha_salida'] . '<br>';
-            echo '</div>';
-        }
-    } else {
-        echo '<p>No hay datos en la tabla "cliente".</p>';
-    }
-    ?>
+            // Verificar la conexión
+            if ($mysqli->connect_error) {
+                die("Error de conexión a la base de datos: " . $mysqli->connect_error);
+            }
 
-    <h1>Personal :</h1>
+            // Consulta para obtener los datos de la tabla 'cliente'
+            $query_cliente = "SELECT * FROM cliente";
+            $result_cliente = $mysqli->query($query_cliente);
 
-    <?php
-    // Consulta para obtener los datos de la tabla 'personal'
-    $query_personal = "SELECT * FROM personal";
-    $result_personal = $mysqli->query($query_personal);
+            // Mostrar resultados
+            if ($result_cliente->num_rows > 0) {
+                echo '<div class="client">';
+                while ($row_cliente = $result_cliente->fetch_assoc()) {
+                    echo '<div>';
+                    echo '<h2>' . $row_cliente['nombre'] . '</h2>';
+                    echo '<div><h3>DNI:</h3>' . $row_cliente['DNI'] . '</div>';
+                    echo '<div><h3>Email:</h3>' . $row_cliente['email'] . '</div>';
+                    echo '<div><h3>Cantidad de Personas:</h3>' . $row_cliente['cantidad_personas'] . '</div>';
+                    echo '<div><h3>Habitación:</h3>' . $row_cliente['habitacion'] . '</div>';
+                    echo '<div><h3>Fecha de Entrada:</h3>' . $row_cliente['fecha_entrada'] . '</div>';
+                    echo '<div><h3>Fecha de Salida:</h3>' . $row_cliente['fecha_salida'] . '</div>';
+                    echo '</div>';
+                }
+                echo '</div>';
+            } else {
+                echo '<p>No hay datos en la tabla "cliente".</p>';
+            }
 
-    // Mostrar resultados
-    if ($result_personal->num_rows > 0) {
-        echo '<p>Total de filas: ' . $result_personal->num_rows . '</p>';
-        while ($row_personal = $result_personal->fetch_assoc()) {
-            echo '<div style="display: inline-block; margin-right: 40px;">';
-            echo 'DNI: ' . $row_personal['DNI'] . '<br>';
-            echo 'Nomnbre: ' . $row_personal['nombre'] . '<br>';
-            echo 'E-mail: ' . $row_personal['email'] . '<br>';
-            echo 'Puesto de Trabajo: ' . $row_personal['puesto'] . '<br>';
-            echo 'Sección laboral: ' . $row_personal['seccion'] . '<br>';
-            echo '</div>';
-            
-        }
-    } else {
-        echo '<p>No hay datos en la tabla "personal".</p>';
-    }
+            // Cerrar la conexión a la base de datos
+            $mysqli->close();
+            ?>
+        </div>
 
-    // Cerrar la conexión a la base de datos
-    $mysqli->close();
-    ?>
+        <div class="section">
+            <h1>Personal:</h1>
+            <?php
+            // Conexión a la base de datos MySQL
+            $mysqli = new mysqli("mysql", "admin", "admin123", "elindio");
+
+            // Verificar la conexión
+            if ($mysqli->connect_error) {
+                die("Error de conexión a la base de datos: " . $mysqli->connect_error);
+            }
+
+            // Consulta para obtener los datos de la tabla 'personal'
+            $query_personal = "SELECT * FROM personal";
+            $result_personal = $mysqli->query($query_personal);
+
+            // Mostrar resultados
+            if ($result_personal->num_rows > 0) {
+                echo '<div class="employee">';
+                while ($row_personal = $result_personal->fetch_assoc()) {
+                    echo '<div>';
+                    echo '<h2>' . $row_personal['nombre'] . '</h2>';
+                    echo '<div><h3>DNI:</h3>' . $row_personal['DNI'] . '</div>';
+                    echo '<div><h3>Email:</h3>' . $row_personal['email'] . '</div>';
+                    echo '<div><h3>Puesto de Trabajo:</h3>' . $row_personal['puesto'] . '</div>';
+                    echo '<div><h3>Sección Laboral:</h3>' . $row_personal['seccion'] . '</div>';
+                    echo '</div>';
+                }
+                echo '</div>';
+            } else {
+                echo '<p>No hay datos en la tabla "personal".</p>';
+            }
+
+            // Cerrar la conexión a la base de datos
+            $mysqli->close();
+            ?>
+        </div>
+    </div>
 </body>
 </html>
